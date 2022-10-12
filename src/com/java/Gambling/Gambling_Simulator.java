5090c74b3a99a39c2602351ffa052bea3ef926c6
shadab <me.shadab19@gmail.com>
2022-10-12 21:21:13 +0530

    [Added - showing stake]

--- src/com/java/Gambling/Gambling_Simulator.java
+++ src/com/java/Gambling/Gambling_Simulator.java
@@ -1,7 +1,13 @@
 package com.java.Gambling;
 
 public class Gambling_Simulator {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Gambling Simulator");
-	}
+	 public static final  int initialStake = 100;
+	    public static final int stakeBet = 1;
+
+	    public static void main(String[] args)
+	    {
+	    	System.out.println("Welcome to Gambling Game Simulator program");
+	        System.out.println("Initial Stake is " +initialStake +"$");
+	        System.out.println("Stake Bet is " +stakeBet+"$");
+	    }
 }
