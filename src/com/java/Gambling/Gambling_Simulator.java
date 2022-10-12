57a92d74271b527bdb8232997f54ee78cb83ac19
shadab <me.shadab19@gmail.com>
2022-10-12 21:25:44 +0530

    [Added - checking  win loose ]

--- src/com/java/Gambling/Gambling_Simulator.java
+++ src/com/java/Gambling/Gambling_Simulator.java
@@ -1,13 +1,26 @@
 package com.java.Gambling;
-
+import java.util.Random;
 public class Gambling_Simulator {
-	 public static final  int initialStake = 100;
-	    public static final int stakeBet = 1;
+	public static final  int initialStake = 100;
+	 public static final int stakeBet = 1;
+	 static Random random = new Random();   
 
 	    public static void main(String[] args)
 	    {
+	    	int totalStake = initialStake;
 	    	System.out.println("Welcome to Gambling Game Simulator program");
-	        System.out.println("Initial Stake is " +initialStake +"$");
-	        System.out.println("Stake Bet is " +stakeBet+"$");
+	    	int play = random.nextInt(2);
+	    	switch(play)
+	    	{
+	    	case 0:
+	    		  totalStake = totalStake - stakeBet;
+	    		  System.out.println("Stake after losing: "+totalStake);
+	    		  break;
+	    	case 1:
+	    		 totalStake = totalStake + stakeBet;
+	    		  System.out.println("Stake after winning: "+totalStake);
+	    		  break;
+	    	}
+	    	
 	    }
 }
